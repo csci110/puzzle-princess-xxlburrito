39a8767d18522a57ac44c69f0fd8f9dae06bff77
N Altman <altmann@dewv.edu>
2018-11-05 16:50:28 +0000

    Updates via sync script

--- puzzledPrincess.js
+++ puzzledPrincess.js
@@ -2,20 +2,44 @@ import {game, Sprite} from "./sgc/sgc.js";
 game.setBackground("floor.png");
 
 class Marker extends Sprite {
-    constructor(game, imageFile, name) {
+    constructor(board, image, name) {
         super();
-        this.game = game;
-        this.setImage(imageFile);
+        this.board = board;
+        this.setImage(image);
         this.name = name;
-        this.x = 150;
-        this.y = 275;
+        this.x = this.startX = 150;
+        this.y = this.startY = 275;
     }
 }
 class PrincessMarker extends Marker {
     constructor(board) {
-        super(board);
-        this.board = board;
-        this.
+        super(board, "annFace.png", "Princess Ann");
+        this.dragging = false;
+    }
+    handleMouseLeftButtonDown() {
+        this.dragging = true;
+    }
+    handleMouseLeftButtonUp() {
+        this.dragging = false;
+        let row = Math.floor((this.y - 85) / 150);
+        let col = Math.floor((this.x - 300) / 150);
+        // window.alert("Row " + row + ", Col " + col);
+        if (row < 0 || row > 2) {
+            this.x = this.startX;
+            this.y = this.startY;
+            return true;
+        }
+        if (col < 0 || col > 2) {
+            this.x = this.startX;
+            this.y = this.startY;
+            return true;
+        }
+    }
+    handleGameLoop() {
+        if(this.dragging ===true) {
+            this.x = game.getMouseX() - this.width / 2;
+            this.y = game.getMouseY() - this.height / 2;
+        }
     }
 }
 class StrangerMarker extends Marker {
